5e1dbf79375a51bfc6ea39f9a49e50a45e165e99
Alec Martin <alecmartin242@gmail.com>
2022-06-20 00:25:59 -0700

    worker started

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Typography } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+
+const worker = new Worker("worker.js");
+worker.onmessage = function(m) {
+  let data = m.data;
+  console.log("worker sent " + data);
+}
 
 export default function App() {
+  const [msg, setMsg] = useState("");
+
+  function sendMessage() {
+    console.log("sending " + msg);
+    worker.postMessage(msg);
+  }
+
   return (
     <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          App goes here
-        </Typography>
-      </Box>
+      <FormControl sx={{ m: 1}} variant="outlined">
+        <InputLabel htmlFor="to-worker">message to worker</InputLabel>
+        <OutlinedInput
+          id="to-worker"
+          type="text"
+          onChange={(e) => setMsg(e.target.value)}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="send to worker"
+                onClick={sendMessage}
+              >
+                <SendIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+        ></OutlinedInput>
+      </FormControl>
+      <Typography>Messages are currently logged in the browser console. Next step: make them visible.</Typography>
     </Container>
   );
 }
